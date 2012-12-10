39ffce289ad282b2e439f743bbfe4efe455b223d
Jorge Israel Peña <jorgepblank@gmail.com>
2012-12-10 01:07:03 -0800

    implemented "equalizer" bars

--- Visualizer/Audio.hs
+++ Visualizer/Audio.hs
@@ -2,10 +2,12 @@ module Visualizer.Audio (
   connectPulse,
   disconnectPulse,
   Buffer,
+  Polars,
+  Spectrum,
+  Sample,
   defaultMonitor,
   bufferSize,
-  readSamples,
-  normalize
+  readSamples
 ) where
 
 -- sound
@@ -28,8 +30,10 @@ import Control.Monad (forM_)
     arecord -L
 -} 
 
+type Sample = Int16
 type Buffer = [Int16]
-type ComplexArray = CA.CArray Int (Complex Double)
+type Spectrum = CA.CArray Int (Complex Double)
+type Polars = CA.CArray Int Double
 
 defaultMonitor :: Maybe String
 defaultMonitor = Just "alsa_output.pci-0000_00_1b.0.analog-stereo.monitor"
@@ -40,16 +44,16 @@ bufferSize = 4096
 connectPulse = Pulse.simpleNew Nothing "Visualizer" Pulse.Record defaultMonitor "A PulseAudio visualizer written in Haskell" (Pulse.SampleSpec (Pulse.S16 Pulse.LittleEndian) 44100 1) Nothing Nothing
 disconnectPulse = Pulse.simpleFree
 
-readSamples :: Pulse.Simple -> [(Buffer -> IO ())] -> IO ()
+readSamples :: Pulse.Simple -> [(Polars -> IO ())] -> IO ()
 readSamples source actions =
   do buffer <- Pulse.simpleRead source bufferSize :: IO Buffer
      let frequencies = polarMagnitudes $ frequencySpectrum buffer
 
-     -- pass the buffer (later will be just the fourier transform) to every action that wants it
-     forM_ actions $ \action -> action buffer
+     -- pass the spectrum
+     forM_ actions $ \action -> action frequencies
 
 -- general purpose sound generating function
-sound :: Double -> Int -> Double -> Int16 -> [Int16]
+sound :: Double -> Int -> Double -> Sample -> Buffer
 sound freq samples len volume = take (round $ len * (fromIntegral samples)) $  -- take out desired number of samples
                          map (round . (* fromIntegral volume)) $ -- scale by the volume
                          map sin [0.0, (freq * 2 * pi / (fromIntegral samples))..] -- generate sample
@@ -59,23 +63,16 @@ sound freq samples len volume = take (round $ len * (fromIntegral samples)) $  -
      -- Pulse' simpleReadRaw
      -- CArray's unsafeByteStringToCArray
      -- IArray's // to modify indices in-place
-frequencySpectrum :: Buffer -> ComplexArray
+frequencySpectrum :: Buffer -> Spectrum
 frequencySpectrum buffer =
   -- calculate the fourier transform
   let len = length buffer
-      complex = (CA.listArray (0, len - 1) $ map (\sample -> (fromIntegral sample) :+ 0) buffer) :: ComplexArray
+      complex = (CA.listArray (0, len - 1) $ map (\sample -> (fromIntegral sample) :+ 0) buffer) :: Spectrum
   in FFTW.dft complex
 
-polarMagnitudes :: ComplexArray -> CA.CArray Int Double
+polarMagnitudes :: Spectrum -> Polars
 polarMagnitudes spectrum =
   CA.sliceWith region region polarConversion spectrum
   where polarConversion (r :+ i) = sqrt (r^2 + i^2)
         region = (0, (bufferSize - 1)) -- we only need up to N / 2 - 1
-
-fiveBins :: CA.CArray Int Double -> [Double]
-fiveBins polars = undefined
-
-normalize :: Buffer -> Int
-normalize buffer = let ints = map (fromIntegral . abs) buffer :: [Int]
-                   in round ((fromIntegral (sum ints `div` fromIntegral (length buffer))) / 32767 * 100)
 
--- Visualizer/Graphics.hs
+++ Visualizer/Graphics.hs
@@ -1,5 +1,6 @@
 module Visualizer.Graphics (
-  renderBars
+  renderBars,
+  printPolars
 ) where
 
 -- graphics
@@ -7,32 +8,41 @@ import qualified Graphics.UI.SDL as SDL
 import qualified Graphics.UI.SDL.Primitives as SDL.P
 
 -- misc
+import qualified Data.Array.CArray as CA
+import qualified Data.Array.IArray as IA
 import Data.Bits (shiftL, (.|.))
 import Data.Word (Word8, Word32)
-import Visualizer.Audio (Buffer, normalize)
+import Visualizer.Audio (Buffer, Polars, Sample, Spectrum)
 import Control.Monad (void)
+import Data.Int (Int16) -- S16 in SampleSpec
 
-renderBars :: SDL.Surface -> Buffer -> IO ()
-renderBars surface buffer =
+renderBars :: SDL.Surface -> Polars -> IO ()
+renderBars surface spectrum =
   -- renderBar takes coords in SDL conventional coordinates
-  do mapM_ (renderBar surface normalized) $ barSpan [10, 40, 70, 100, 130]
+  do mapM_ (uncurry $ renderBar surface) $ zip (CA.elems spectrum) (barSpan [10, 40, 70, 100, 130, 160, 190, 220])
   where barSpan points = map (\point -> (point, (480 - 10))) points
-        normalized = normalize buffer
 
 -- render volume bar in SDL
-renderBar :: SDL.Surface -> Int -> (Int, Int) -> IO ()
-renderBar surface normalized (sdl_x, sdl_y) =
+renderBar :: SDL.Surface -> Double -> (Int, Int) -> IO ()
+renderBar surface level (sdl_x, sdl_y) =
   -- the SDL.Rect box accepts is x1 y1 x2 y2 where 1's are top right, 2's are bottom left
   do void $ SDL.P.box surface (SDL.Rect x y w h) (SDL.Pixel 0xFFFFFFFF) -- (color 255 255 255 255)
   where x = sdl_x + 20
         y = sdl_y
         w = sdl_x
-        h = y - normalized
+        h = y - (round normalized)
+        normalized = level / (fromIntegral (maxBound :: Int16))
 
+{- 
 -- print volume bar to terminal
 printAverageAmplitude :: Buffer -> IO ()
 printAverageAmplitude buffer =
   do putStrLn (replicate (normalize buffer) '*')
+-}
+
+printPolars :: Polars -> IO ()
+printPolars polars =
+  do putStrLn $ show polars
 
 -- for sdl_gfx primitives; SDL.mapRGBA seems to be broken
 -- just a convenience method to call with decimal integer values: color 255 255 255 255
